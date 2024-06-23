28d7858eab6583f5f3c05aed79c39a2f49eeb59f
arafatGithubCode <md489288@gmail.com>
2024-06-23 10:57:43 +0600

    update spinner

--- src/components/custom/Spinner.tsx
+++ src/components/custom/Spinner.tsx
@@ -1,7 +1,10 @@
+import spinner from "../../assets/spinner.svg";
 const Spinner = () => {
   return (
-    <div className="flex justify-center items-center h-screen w-full">
-      <div className="w-10 h-10 border-t-2 border-b-2 border-dotted border-gray-500 animate-spin rounded-full"></div>
+    <div className="flex justify-center items-center z-50 top-0 bottom-0 right-0 left-0 fixed bg-black bg-opacity-50">
+      <div>
+        <img src={spinner} alt="Loading" className="h-24" />
+      </div>
     </div>
   );
 };
