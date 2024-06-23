38ce4502903f3757caf0b1806abdfa5d586bb8a6
arafatGithubCode <md489288@gmail.com>
2024-06-23 12:10:02 +0600

    create update my restaurant request

--- src/api/MyRestaurantApi.tsx
+++ src/api/MyRestaurantApi.tsx
@@ -71,3 +71,42 @@ export const useCreateMyRestaurant = () => {
 
   return { createRestaurant, isLoading };
 };
+
+export const useUpdateMyRestaurant = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const updateMyRestaurantRequest = async (
+    restaurantFormData: FormData
+  ): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${VITE_API_BASE_URL}/api/my/restaurant`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: restaurantFormData,
+    });
+
+    if (!response) {
+      throw new Error("Failed to update restaurant");
+    }
+
+    return response.json();
+  };
+
+  const {
+    mutate: updateRestaurant,
+    isLoading,
+    error,
+    isSuccess,
+  } = useMutation(updateMyRestaurantRequest);
+
+  if (error) {
+    toast.error("Unable to update restaurant");
+  }
+  if (isSuccess) {
+    toast.success("Restaurant updated successfully!");
+  }
+
+  return { isLoading, updateRestaurant };
+};
--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,19 +1,25 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  const { createRestaurant, isLoading: createLoadingRestaurant } =
+    useCreateMyRestaurant();
   const { restaurant } = useGetMyRestaurant();
+  const { updateRestaurant, isLoading: updateLoadingRestaurant } =
+    useUpdateMyRestaurant();
+
+  const isEditing = !!restaurant;
 
   return (
     <>
       <ManageRestaurantForm
         restaurant={restaurant}
-        onSave={createRestaurant}
-        isLoading={isLoading}
+        onSave={isEditing ? updateRestaurant : createRestaurant}
+        isLoading={createLoadingRestaurant || updateLoadingRestaurant}
       />
     </>
   );
