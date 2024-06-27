ea003799442b15f7126cab179f9cc5b3366be5b6
arafatGithubCode <md489288@gmail.com>
2024-06-27 17:08:11 +0600

    create restaurant orders page

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -3,6 +3,9 @@ import {
   useGetMyRestaurant,
   useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
+import { useGetMyOrders } from "@/api/OrderApi";
+import OrderItemCard from "@/components/custom/OrderItemCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
@@ -11,16 +14,37 @@ const ManageRestaurantPage = () => {
   const { restaurant } = useGetMyRestaurant();
   const { updateRestaurant, isLoading: updateLoadingRestaurant } =
     useUpdateMyRestaurant();
+  const { orders } = useGetMyOrders();
 
   const isEditing = !!restaurant;
 
   return (
     <>
-      <ManageRestaurantForm
-        restaurant={restaurant}
-        onSave={isEditing ? updateRestaurant : createRestaurant}
-        isLoading={createLoadingRestaurant || updateLoadingRestaurant}
-      />
+      <Tabs defaultValue="orders">
+        <TabsList>
+          <TabsTrigger value="orders">Orders</TabsTrigger>
+          <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+        </TabsList>
+        <TabsContent
+          value="orders"
+          className="space-y-5 bg-gray-50 p-10 rounded-lg"
+        >
+          <h2 className="text-2xl font-bold">
+            {orders?.length}{" "}
+            {orders?.length === 1 ? "active order" : "active orders"}
+          </h2>
+          {orders?.map((order) => (
+            <OrderItemCard order={order} />
+          ))}
+        </TabsContent>
+        <TabsContent value="manage-restaurant">
+          <ManageRestaurantForm
+            restaurant={restaurant}
+            onSave={isEditing ? updateRestaurant : createRestaurant}
+            isLoading={createLoadingRestaurant || updateLoadingRestaurant}
+          />
+        </TabsContent>
+      </Tabs>
     </>
   );
 };
