233feb7d06ba90503fffa2eb6663030848dd333b
arafatGithubCode <md489288@gmail.com>
2024-06-27 11:27:54 +0600

    add header of order status page

--- src/components/custom/OrderStatusHeader.tsx
+++ src/components/custom/OrderStatusHeader.tsx
@@ -4,10 +4,25 @@ type Props = {
   order: Order;
 };
 const OrderStatusHeader = ({ order }: Props) => {
+  const getExpectedDelivery = () => {
+    const created = new Date(order.createdAt);
+
+    created.setMinutes(
+      created.getMinutes() + order.restaurant.estimatedDeliveryTime
+    );
+
+    const hours = created.getHours();
+    const minutes = created.getMinutes();
+
+    const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    return `${hours}:${paddedMinutes}`;
+  };
   return (
     <>
-      <h1 className="text-4xl font-bold tracking-tighter flex flex-col gap-5 md:flex-row md:justify-between">
-        <span>Order Status</span>
+      <h1 className="md:text-4xl text-3xl font-bold tracking-tighter flex flex-col gap-5 md:flex-row md:justify-between">
+        <span>Order Status: {order.status}</span>
+        <span>Expected By: {getExpectedDelivery()}</span>
       </h1>
     </>
   );
